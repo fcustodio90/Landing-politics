76dc051e626fe28fc1b0e094f214aaac57812992
filipe custodio <filipe.l.custodio@gmail.com>
2019-02-04 00:01:05 +0000

    environment.js fix for heroku deploy

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,23 +1,15 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 
-environment.plugins.prepend(
-  'Provide',
+
+environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
-    jquery: 'jquery',
     'window.jQuery': 'jquery',
+    'window.$': 'jquery',
     Popper: ['popper.js', 'default']
   })
 )
-
-environment.loaders.append('expose', {
-  test: require.resolve('jquery'),
-  use: [{
-    loader: 'expose-loader',
-    options: '$'
-  }]
-})
 
 module.exports = environment
