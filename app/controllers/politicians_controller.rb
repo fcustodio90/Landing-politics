1116d506a381ba14c5f1d7bb742502d47e3bad11
filipe custodio <filipe.l.custodio@gmail.com>
2019-02-04 19:01:32 +0000

    styling refactor

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -1,8 +1,16 @@
 class PoliticiansController < ApplicationController
+  before_action :set_politician, only: [:show]
+
   def index
     @politicians = Politician.all
   end
 
   def show
+
+  end
+
+
+  def set_politician
+    @politician = Politician.find(params[:id])
   end
 end
